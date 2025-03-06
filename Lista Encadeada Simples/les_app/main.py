582874e7c722fbd4eb41a1007824e33b223e63c0
goncaze <goncaze@gmail.com>
2025-03-06 16:20:24 -0300

    Commit 3

--- Lista Encadeada Simples/les_app/main.py
+++ Lista Encadeada Simples/les_app/main.py
@@ -2,7 +2,15 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    # Configurar tema de cor da page
+    page.theme_mode = ft.ThemeMode.LIGHT
+    # Setar largura da page
+    page.window.width = 350
+    # Setar altura da page
+    page.window.height = 630
+
+    # Atualizar/Recarregar a page
+    page.update()
 
 
 ft.app(main)
