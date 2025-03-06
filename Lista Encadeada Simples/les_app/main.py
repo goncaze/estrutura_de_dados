d22c5d7221dedef107061145b36e1d71bfab66b8
goncaze <goncaze@gmail.com>
2025-03-06 16:24:49 -0300

    Commit 4

--- Lista Encadeada Simples/les_app/main.py
+++ Lista Encadeada Simples/les_app/main.py
@@ -5,9 +5,13 @@ def main(page: ft.Page):
     # Configurar tema de cor da page
     page.theme_mode = ft.ThemeMode.LIGHT
     # Setar largura da page
-    page.window.width = 350
+    page.window.width = 380
     # Setar altura da page
-    page.window.height = 630
+    page.window.height = 680
+
+    txt_titulo = ft.Text(value="Lista Encadeada Simples", size=25)
+
+    page.add(txt_titulo)
 
     # Atualizar/Recarregar a page
     page.update()
