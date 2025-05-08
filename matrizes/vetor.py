0f5bc27a3ca46900ca0296209e3d88771ce0cbd7
goncaze <goncaze@gmail.com>
2025-05-08 16:53:35 -0300

    Arrays 3D

--- matrizes/vetor.py
+++ matrizes/vetor.py
@@ -15,3 +15,21 @@ print(lista)
 print(f"{type(lista) = }")
 
 print()
+
+array_zero_d = np.array(180)
+print(array_zero_d)
+print(f"{type(array_zero_d) = }")
+
+print()
+
+array_dois_d = np.array([[1, 2], [7, 9]])
+print(array_dois_d)
+print(f"{type(array_dois_d) = }")
+
+print()
+
+array_tres_d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(array_tres_d)
+print(f"{type(array_tres_d) = }")
+
+print()
