55798097fd243d51d5aa4f36320f3aa63f6c6524
goncaze <goncaze@gmail.com>
2025-05-08 16:35:13 -0300

    Matrizes

---
+++ matrizes/vetor.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# print(np.__version__)
+print()
+
+arr = np.array([1, 2, 3, 4, 5])  # LISTA
+arr = np.array((1, 2, 3, 4, 5))  # TUPLA
+print(arr)
+print(f"{type(arr) = }")
+
+print()
+
+lista = [12, 23, 34, 45, 56]
+print(lista)
+print(f"{type(lista) = }")
+
+print()
